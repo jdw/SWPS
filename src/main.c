ad7dc93656796b754005b82c6482fd28225dce00
Johannes D. Wirde <johannes.wirde@gmail.com>
2011-08-17 20:56:48 +0200

    Removing all commented out code (its in the rep anywho) and adding UI.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <define.h>
+
 #include <SFML/Audio.h>
 #include <SFML/Graphics.h>
 #include <stdio.h>
 #include <SFML/Graphics/Font.h>
+
+#include "ui.h"
 
 static sfFont* g_debugFont = 0;
 static sfWindowSettings WINDOW_SETTINGS = {24, 8, 0}; 
@@ -9,26 +13,15 @@ static sfVideoMode VIDEO_MODE = {800, 600, 32};
 static sfRenderWindow* APP = 0;
 static sfSprite* g_pWall;
 static sfSprite* g_pFloor;
-
-typedef enum {
-	AOK,
-	NO_DEBUG_FONT,
-	FAILED_VIDEO_MODE,
-	FAILED_LOADING_FONT_FILE
-} F_EXIT;
+static sfSprite* g_pPlayer;
+static sfSprite* g_pNPC;
+static F_UI* g_pUI;
 
 static int
 init() {
 	// Create the main window
-//	int t_modeCount = sfVideoMode_GetModesCount();
-//	printf("Graphics mode count: %d\n", t_modeCount);
-//	for (int i = 0; i < t_modeCount; ++i) {
-//		sfVideoMode t_mode = sfVideoMode_GetMode(i);
-//		printf("%dx%d, %dbpp\n", t_mode.Width, t_mode.Height, t_mode.BitsPerPixel);
-//	}
-
-        APP = sfRenderWindow_Create(VIDEO_MODE, "SWPS", sfResize | sfClose, WINDOW_SETTINGS);
-        if (!APP) return EXIT_FAILURE;
+	APP = sfRenderWindow_Create(VIDEO_MODE, "SWPS", sfResize | sfClose, WINDOW_SETTINGS);
+	if (!APP) return EXIT_FAILURE;
 
 	// Loading debug font
 	g_debugFont = sfFont_CreateFromFile("ass/FreeSerif.ttf", 50, NULL);
@@ -36,17 +29,26 @@ init() {
 
 	// Wall and floor graphics
 	sfImage* t_pImage = sfImage_CreateFromFile("ass/wall_stone.png");
-        if (!t_pImage) return EXIT_FAILURE;
-        g_pWall = sfSprite_Create();
-        sfSprite_SetImage(g_pWall, t_pImage);
-//	sfImage_Destroy(t_pImage);
+	if (!t_pImage) return EXIT_FAILURE;
+	g_pWall = sfSprite_Create();
+	sfSprite_SetImage(g_pWall, t_pImage);
 
 	t_pImage = sfImage_CreateFromFile("ass/dirt_floor.png");
-        if (!t_pImage) return EXIT_FAILURE;
-        g_pFloor = sfSprite_Create();
-        sfSprite_SetImage(g_pFloor, t_pImage);
-//	sfImage_Destroy(t_pImage);
+	if (!t_pImage) return EXIT_FAILURE;
+	g_pFloor = sfSprite_Create();
+	sfSprite_SetImage(g_pFloor, t_pImage);
 	
+	// Player and NPC
+	t_pImage = sfImage_CreateFromFile("ass/hero.png");
+	if (!t_pImage) return EXIT_FAILURE;
+	g_pPlayer = sfSprite_Create();
+	sfSprite_SetImage(g_pPlayer, t_pImage);
+
+	t_pImage = sfImage_CreateFromFile("ass/enemah.png");
+	if (!t_pImage) return EXIT_FAILURE;
+	g_pNPC = sfSprite_Create();
+	sfSprite_SetImage(g_pNPC, t_pImage);
+
 	return AOK;
 }
 
@@ -68,12 +70,48 @@ F_draw(F_gfxLayers i_layer, sfSprite* i_pAsset, const int i_x, const int i_y) {
 	return AOK;
 }
 
+static int
+F_events(int* i_q, int* i_x, int* i_y) {
+	sfEvent t_event;
+	
+	while (sfRenderWindow_GetEvent(APP, &t_event)) {
+		if (t_event.Type == sfEvtKeyPressed) {
+			switch (t_event.Key.Code) {
+				case sfKeyEscape: *i_q = 1; break;
+				case sfKeyNumpad8:
+					*i_x -=  0; *i_y -= 16;
+					fUI_setMessage(g_pUI, "Hero moves up!");
+				break;
+				case sfKeyNumpad9: *i_x += 16; *i_y -= 16; break;
+				case sfKeyNumpad6: *i_x += 16; *i_y +=  0; break;
+				case sfKeyNumpad3: *i_x += 16; *i_y += 16; break;
+				case sfKeyNumpad2: *i_x -=  0; *i_y += 16; break;
+				case sfKeyNumpad1: *i_x -= 16; *i_y += 16; break;
+				case sfKeyNumpad4: *i_x -= 16; *i_y -=  0; break;
+				case sfKeyNumpad7: *i_x -= 16; *i_y -= 16; break;
+				default: break;
+			}
+		}
+	}
+
+	// Close window : exit
+	if (t_event.Type == sfEvtClosed || *i_q) sfRenderWindow_Close(APP);
+    
+	return AOK;
+}
+
 int
 main() {
-	//sfImage* t_pImage;
+	int t_initReturn;
+	if ((t_initReturn = init())) {
+		printf("init() failed with code: %d\n", t_initReturn);
+		return t_initReturn;
+	}
+
 	sfString* t_pText = 0;
-	sfSprite* t_pSprite = 0;
-	char *t_pMap[]={
+	g_pUI = F_UI_create();
+
+	char *t_pMap[] = {
 		"###############",
 		"#      #      #",
 		"#             #",
@@ -86,42 +124,37 @@ main() {
 		"###############"
 	};
 
-	sfEvent t_event;
-	int t_quit = 0;
+																																																							int t_quit = 0;
+	int t_x = 100 + 4 * 16;
+	int t_y = 100 + 3 * 16;
 	
-     
-	int t_initReturn;
-        if ((t_initReturn = init())) {
-                printf("init() failed with code: %d\n", t_initReturn);
-                return t_initReturn;
-        }
 
 	// Create a graphical string to display 
 	t_pText = sfString_Create();
 	sfString_SetFont(t_pText, g_debugFont);
 	sfString_SetText(t_pText, "Hello");
-	sfString_SetSize(t_pText, 50);
-	sfColor t_color = {200,200,200,205};
+	sfString_SetSize(t_pText, 15);
+	sfString_SetPosition(t_pText, 100, 100);
+	sfColor t_color = {0,0,0,205};
 	sfString_SetColor(t_pText, t_color);
- 
+	sfClock* t_pClock = sfClock_Create();
+	sfClock_Reset(t_pClock);
+	float t_last = sfClock_GetTime(t_pClock);	
+
+	fUI_setMessage(g_pUI, "hej");
+
+	int t_frameCount = 0;
 	// Start the game loop
 	while (sfRenderWindow_IsOpened(APP)) {
 		// Process events
-		while (sfRenderWindow_GetEvent(APP, &t_event)) {
-			if (t_event.Type == sfEvtKeyPressed) {
-				if (t_event.Key.Code == sfKeyEscape) {
-					t_quit = 1;
-				}
-			}
- 
-			// Close window : exit
-			if (t_event.Type == sfEvtClosed || t_quit)
-                 sfRenderWindow_Close(APP);
-         }
+		F_events(&t_quit, &t_x, &t_y);
  		
 		// Clear the screen
 		sfRenderWindow_Clear(APP, sfBlack);
 		
+		// Update UI
+		F_UI_update(g_pUI);
+
 		// Draw the map
 		for (int y = 0; y < 15; ++y)
 			for (int x = 0; x < 10; ++x) {
@@ -129,11 +162,19 @@ main() {
 				int t_yPos = 100 + y * 16;
 				F_draw(MAP, (t_pMap[x][y] == '#')? g_pWall: g_pFloor, t_xPos, t_yPos);	
 			}
-		// Draw the string
-		sfRenderWindow_DrawString(APP, t_pText);
+
+		// Draw enemy
+		F_draw(NPC, g_pNPC, 100 + 3 * 16, 100 + 3 * 16);
+		
+		// Draw player
+		F_draw(PLAYER, g_pPlayer, t_x, t_y); 
+		
+		// Draw UI
+		F_UI_draw(g_pUI, APP);
+
 		// Update the window
 		sfRenderWindow_Display(APP);
-     }
+   	 }
  
 	// Cleanup resources
 	sfString_Destroy(t_pText);
