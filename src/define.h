097c73a3832ece69b4307f3703fcfc4b70bb981c
Johannes D. Wirde <johannes.wirde@gmail.com>
2011-08-28 13:28:27 +0200

    Syntactic corrections.

--- src/define.h
+++ src/define.h
@@ -2,10 +2,18 @@
 #define F_DEFINE_H
 
 typedef enum {
-    AOK,
-    NO_DEBUG_FONT,
-    FAILED_VIDEO_MODE,
-    FAILED_LOADING_FONT_FILE
+	AOK,
+	// Exit reasons for program
+	NO_DEBUG_FONT,
+	FAILED_VIDEO_MODE,
+	FAILED_LOADING_FONT_FILE,
+	// Reason why a tile could be blocked
+	TILE_BLOCKED_ITEM,
+	TILE_BLOCKED_ACTOR,
+	TILE_BLOCKED_TYPE,
+	// Map
+	MAP_POS_X_OUTSIDE,
+	MAP_POS_Y_OUTSIDE
 } F_EXIT;
 
 #endif // F_DEFINE_H
